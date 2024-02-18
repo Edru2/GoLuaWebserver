82dc01c276941e668a6487ea6db4ec5bb4db6069
Manuel Wegmann <60551052+Edru2@users.noreply.github.com>
2024-02-18 14:29:17 +0100

    try: new release (#1)
    * delete some files
    
    * request as table
    
    * use luajit as lua link
    
    * fix: headers were always nil
    
    * Update .gitignore
    
    * add file serving capability
    
    * minor adjustments (goPath and mutex in websockets)
    
    * try to repair the release action

--- Examples/WebTest.lua
+++ Examples/WebTest.lua
@@ -1,24 +1,49 @@
 package.path = package.path .. ";../?.so"
+local etlua = require("Examples.etlua")
 local web = require("goLuaWebserver")
 local serverId = web.startWebserver("localhost:8080")
-
-io.write("HTML:")
-local html = io.read()
-local function handleRequest(method, path)
-	print(method)
-	print(path)
-    return  200, html, {["Content-Type"] = "text/html"}
+local template = etlua.compile([[
+  <body style="color: rgb(150,150,50); background: rgb(50,50,50);">
+  Hello <%= name %>,
+  Here are your items:
+  <% for i, item in pairs(items) do %>
+   <ul>* <%= i %> = <%= item %></ul>
+  <% end %>
+  </body>
+]])
+web.serveFiles(serverId, "/", [[./Examples/]])
+io.write("Name:")
+local name = io.read()
+local function handleRequest(request)
+	local everythang = {}
+	for k, v in pairs(request) do
+		if type(v) ~= "table" then
+			everythang[k] = v
+			print(string.format("%s = %s", k, v))
+		end
+		if type(v) == "table" then
+			print(k)
+			for k1, v1 in pairs(v) do
+				if type(v1) == "string" then
+					everythang[k1] = v1
+					print(string.format("		%s = %s", k1, v1))
+				end
+			end
+		end
+	end
+	local html = template({ name = name, items = everythang })
+	return 200, html, { ["Content-Type"] = "text/html" }
 end
 
 
 local function handleSocket(client, messagetype, message)
-	print(string.format("Client: %s MT: %d Message: %s",client, messagetype, message))
+	print(string.format("Client: %s MT: %d Message: %s", client, messagetype, message))
 	web.writeWebSocket(serverId, client, message)
 end
 
 io.write("Path:")
 local path = io.read()
-web.serve(serverId, string.format("/%s",path), handleRequest)
+web.serve(serverId, string.format("/%s", path), handleRequest)
 web.serveWebSocket(serverId, "/ws", handleSocket)
 io.read()
 web.stopWebserver(serverId)
